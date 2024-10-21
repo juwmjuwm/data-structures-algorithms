d2c0a0e48f95992828a809ca56bae4cfc84f9a23
Cavarano <maciek.kala.mk@wp.pl>
2024-10-21 12:47:54 +0200

    add structures directory

---
+++ algorithms/structures/LinkedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms.structures
+{
+    internal class LinkedList<T>
+    {
+        private Node<T> Head { get; set; }
+        private Node<T> Tail { get; set; }
+        private int Count { get; set; }
+
+        public void AddFirst(T value)
+        {
+
+        }
+    }
+}
---
+++ algorithms/structures/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms.structures
+{
+    internal class Node<T>
+    {
+
+        public Node<T> Next;
+        public Node<T> Prev;
+        public T Data;
+    }
+}
