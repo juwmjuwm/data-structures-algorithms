6810421ed073dc1a108097bb8ca70d7874d9e158
juwmjuwm <jacek.nowak.2@student.uwm.edu.pl>
2024-10-28 12:53:04 +0100

    add bst classes

---
+++ algorithms/structures/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿namespace dsa.structures
+{
+    internal class TreeNode<T>
+    {
+        TreeNode<T>? Parent { get; set; }
+        TreeNode<T>? LeftChild { get; set; }
+        TreeNode<T>? RightChild { get; set; }
+        T Data { get; set; }
+
+        public TreeNode(T data)
+        {
+            Parent = null;
+            LeftChild = null;
+            RightChild = null;
+            Data = data;
+        }
+    }
+}
