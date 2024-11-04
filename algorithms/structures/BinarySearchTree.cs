ff54c7efd097a7e15e4ee96825fcc1e8ac7f15da
juwmjuwm <jacek.nowak.2@student.uwm.edu.pl>
2024-11-04 09:07:05 +0100

    bst remove2

--- algorithms/structures/BinarySearchTree.cs
+++ algorithms/structures/BinarySearchTree.cs
@@ -45,6 +45,137 @@
 
         public bool Remove(T value)
         {
+            return Remove(value, Root);
+        }
+
+        public bool Remove(T value, TreeNode<T>? baseNode)
+        {
+            if (Root == null)
+            {
+                return false;
+            }
+            TreeNode<T>? cur = baseNode;
+            while (true)
+            {
+                if (cur == null)
+                {
+                    return false;
+                }
+                else if (value.CompareTo(cur.Data) < 0)
+                {
+                    cur = cur.LeftChild;
+                }
+                else if (value.CompareTo(cur.Data) > 0)
+                {
+                    cur = cur.RightChild;
+                }
+                else // here, cur is the node to remove
+                {
+                    break;
+                }
+            }
+
+            TreeNode<T>? curParent = cur.Parent;
+
+            // option 1: no children
+            if (cur.LeftChild == null && cur.RightChild == null)
+            {
+                if (curParent == null)
+                {
+                    Root = null;
+                }
+                else
+                {
+                    if (cur.Data.CompareTo(curParent.Data) < 0)
+                    {
+                        curParent.LeftChild = null;
+                    }
+                    else if (cur.Data.CompareTo(curParent.Data) > 0)
+                    {
+                        curParent.RightChild = null;
+                    }
+                }
+                //cur.LeftChild = cur.RightChild = cur.Parent = null;
+            }
+
+            // option 2: has left child
+            else if (cur.LeftChild != null && cur.RightChild == null)
+            {
+                if (curParent == null)
+                {
+                    Root = cur.LeftChild;
+                }
+                else
+                {
+                    if (cur.Data.CompareTo(curParent.Data) < 0)
+                    {
+                        curParent.LeftChild = cur.LeftChild;
+                        cur.LeftChild.Parent = curParent;
+                    }
+                    else if (cur.Data.CompareTo(curParent.Data) > 0)
+                    {
+                        curParent.RightChild = cur.LeftChild;
+                        cur.LeftChild.Parent = curParent;
+                    }
+                }
+                //cur.LeftChild = cur.RightChild = cur.Parent = null;
+            }
+
+            // option 3: has right child
+            else if (cur.LeftChild == null && cur.RightChild != null)
+            {
+                if (curParent == null)
+                {
+                    Root = cur.RightChild;
+                }
+                else
+                {
+                    if (cur.Data.CompareTo(curParent.Data) < 0)
+                    {
+                        curParent.LeftChild = cur.RightChild;
+                        cur.RightChild.Parent = curParent;
+                    }
+                    else if (cur.Data.CompareTo(curParent.Data) > 0)
+                    {
+                        curParent.RightChild = cur.RightChild;
+                        cur.RightChild.Parent = curParent;
+                    }
+                }
+                //cur.LeftChild = cur.RightChild = cur.Parent = null;
+            }
+
+            // option 4: has two children
+            else
+            {
+                TreeNode<T> leftmost = cur.RightChild;
+                while (leftmost.LeftChild != null)
+                {
+                    leftmost = leftmost.LeftChild;
+                }
+
+                cur.Data = leftmost.Data;
+                cur.RightChild = leftmost.RightChild;
+                if (leftmost.RightChild != null)
+                {
+                    leftmost.RightChild.Parent = curParent;
+                }
+                //Remove(leftmost.Data, cur.RightChild);
+
+                if (curParent == null)
+                {
+                    Root = cur;
+                }
+            }
+
+            if (Root != null)
+            {
+                Root.Parent = null;
+            }
+            return true;
+        }
+
+        public bool Remove2(T value)
+        {
             if (Root == null)
             {
                 return false;
