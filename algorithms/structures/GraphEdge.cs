890f1074b73c27050cfb0a856a3908b3f8b66c35
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2024-11-25 12:54:35 +0100

    graphs

---
+++ algorithms/structures/GraphEdge.cs
@@ -0,0 +1,17 @@
+﻿namespace dsa.structures
+{
+    internal class GraphEdge
+    {
+
+        public GraphNode Start { get; set; }
+        public GraphNode End { get; set; }
+        public int Weight { get; set; }
+
+        public GraphEdge(GraphNode start, GraphNode end, int weight)
+        {
+            Start = start;
+            End = end;
+            Weight = weight;
+        }
+    }
+}
