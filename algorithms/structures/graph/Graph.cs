340cf97de017ae4668b2a4a85b3cb372e438186f
juwmjuwm <jacek.nowak.2@student.uwm.edu.pl>
2024-12-01 22:56:52 +0100

    add vertex and edge ops

---
+++ algorithms/structures/graph/Graph.cs
@@ -0,0 +1,51 @@
+﻿namespace dsa.structures.graph
+{
+    internal class Graph
+    {
+
+        private int _size;
+        private readonly Dictionary<Vertex, List<Vertex>> _adjacencyList;
+
+        public int Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+
+        public Dictionary<Vertex, List<Vertex>> AdjacencyList
+        {
+            get { return _adjacencyList; }
+        }
+
+        public Graph()
+        {
+            _size = 0;
+            _adjacencyList = new Dictionary<Vertex, List<Vertex>>();
+        }
+
+        public Graph(Edge edge) : this()
+        {
+            Add(edge);
+        }
+
+        public void Add(Vertex vertex)
+        {
+            AdjacencyList.Add(vertex, new List<Vertex>());
+        }
+
+        public void Add(Edge edge)
+        {
+            if (!AdjacencyList.ContainsKey(edge.Start))
+            {
+                AdjacencyList.Add(edge.Start, new List<Vertex>());
+            }
+            AdjacencyList.GetValueOrDefault(edge.Start).Add(edge.End);
+
+            if (!AdjacencyList.ContainsKey(edge.End))
+            {
+                AdjacencyList.Add(edge.End, new List<Vertex>());
+            }
+            AdjacencyList.GetValueOrDefault(edge.End).Add(edge.Start);
+        }
+    }
+}
