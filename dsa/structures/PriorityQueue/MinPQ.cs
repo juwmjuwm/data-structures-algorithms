ee32354127de006bd35f0623cad177ae90d7dddb
juwmjuwm <jacek.nowak.2@student.uwm.edu.pl>
2024-12-16 12:59:38 +0100

    begin implementing MinPQ

---
+++ dsa/structures/PriorityQueue/MinPQ.cs
@@ -0,0 +1,27 @@
+﻿namespace dsa.structures.PriorityQueue
+{
+    /// <summary>
+    /// A binary heap implementation of priority queue.
+    /// </summary>
+    /// <typeparam name="T">The element's key type.</typeparam>
+    public class MinPQ<T> where T : IComparable<T>
+    {
+
+        private T[] _pq;
+        private int _n;
+
+        public MinPQ(int capacity)
+        {
+            _pq = new T[capacity];
+            _n = 0;
+        }
+
+        public void Insert(T key)
+        {
+            // add to the end
+            // then swim up
+            _pq[_n++] = key;
+            Swim(key);
+        }
+    }
+}
